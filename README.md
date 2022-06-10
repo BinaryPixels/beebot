8de41258fd7bdee26aed201db11720d66cd7965e
TheBinaryDigit <hi.binarydigit@gmail.com>
2022-06-10 11:04:52 -0400

    rewrote code for discord js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # beebot
-My first Discord bot
+## My first Discord bot
+![bee](https://tenor.com/bPic4.gif)
+
+### Instructions
+- Create bot application in Discord
+- run npm install --save discord.js dotenv
---
+++ bot.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const Discord = require("discord.js");
+
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on("messageCreate", msg => {
+  if (msg.content === "ping") {
+    msg.channel.send("pong");
+  }
+})
+
+client.login(process.env.DISCORD_TOKEN);
